ae2c7743e5e77adc899cb9d9db901dcdb5d5482b
Mendel Feygelson <mfeygelson@gmail.com>
2019-02-11 19:04:11 -0600

    Updates

---
+++ precache-manifest.766b03ab0d50ba18db2ee60408e3816b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5542b5afed9d4395bb7",
+    "url": "/reddit-reader/static/css/main.ab918435.chunk.css"
+  },
+  {
+    "revision": "a5542b5afed9d4395bb7",
+    "url": "/reddit-reader/static/js/main.a5542b5a.chunk.js"
+  },
+  {
+    "revision": "92b9a3a6c0aa34a0b742",
+    "url": "/reddit-reader/static/js/1.92b9a3a6.chunk.js"
+  },
+  {
+    "revision": "57f29a89d196efff500c",
+    "url": "/reddit-reader/static/js/runtime~main.57f29a89.js"
+  },
+  {
+    "revision": "56e4ecfdf6efd395932170f991318bf5",
+    "url": "/reddit-reader/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50d48063855fc93c8039ec166517af6f.js"
+  "/reddit-reader/precache-manifest.766b03ab0d50ba18db2ee60408e3816b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/reddit-reader/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
